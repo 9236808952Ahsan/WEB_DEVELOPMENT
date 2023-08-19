204d8399cccc4cec22e2f42c73ce1bc97baf8093
Mohd Ahsan <96678067+9236808952Ahsan@users.noreply.github.com>
2023-08-19 23:42:48 +0530

    Add files via upload

---
+++ DOM_Events.js
@@ -0,0 +1,13 @@
+function myfunction()
+{
+    console.log("I was clicked");
+}
+
+const mouseOver=()=>{
+    console.log("mouse over");
+}
+
+function keyPressEvent()
+{
+    console.log('key was pressed');
+}
